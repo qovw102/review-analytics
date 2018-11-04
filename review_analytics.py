5fbd3a29509f70cf6e2dbea4b5ecb6cb6629bb35
Nelson Ye <qovw102@gmail.com>
2018-11-04 15:44:15 +0800

    word count

--- review_analytics.py
+++ review_analytics.py
@@ -9,6 +9,16 @@ with open('reviews.txt', 'r') as f:
         #    print(len(data))
 print('檔案讀取結束共有', len(data), '筆資料')
 
+world_count = {}
+for d in data:
+    words = d.strip().split()
+    #print(words)
+    for word in words:
+        if word in world_count:
+            world_count[word] += 1
+        else:
+            world_count[word] = 1
+
 sum_len = 0
 for d in data:
     sum_len += len(d)
@@ -21,3 +31,18 @@ for s in data:
         new.append(s)
         counter_100 += 1
 print('共有', len(new), '筆資料字數少於100')
+
+for word in world_count:
+    if world_count[word] > 1000000:
+        print(word, world_count[word])
+
+while True:
+    word = input('please enter keyword: ')
+    if word == 'q':
+        print('離開')
+        break
+    if word in world_count:
+        print(word, '出現過次數:', world_count[word])
+    else:
+        print('這個字未出現過!')
+
